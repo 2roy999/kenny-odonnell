a936f2bde47cb7048cb40e13f0378b1234c96e8b
Roy Shmeuli <roysh9@gmail.com>
2018-06-06 16:24:58 +0300

    Add reminder for the duty

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict'
 
+const { DateTime } = require('luxon')
+
 const { Gsheet } = require('./lib/data/gsheet')
 const { KeyValueStore } = require('./lib/data/key-value')
 const { DutiesManager } = require('./lib/data/duties')
 const { UserManager } = require('./lib/data/users')
 const { TelegramBot } = require('./lib/data/telegram')
+const { remindDuty } = require('./lib/reminders/duties')
 
 const gsheet = new Gsheet({
   credentials: process.env.GOOGLE_CREDENTIALS,
@@ -18,23 +21,28 @@ const keyValueStore = new KeyValueStore({
   environment: process.env.ENVIRONMENT
 })
 
-const dutiesManager = new DutiesManager({
-  gsheet,
-  dateFormat: process.env.DATASTORE_DATE_FORMAT
-})
-
 const userManager = new UserManager({
   keyValueStore
+})
+
+const dutiesManager = new DutiesManager({
+  gsheet,
+  userManager,
+  dateFormat: process.env.DATASTORE_DATE_FORMAT
 })
 
 const telegramBot = new TelegramBot({
   apiKey: process.env.TELEGRAM_API_KEY
 })
 
+const dependencies = {
+  gsheet,
+  keyValueStore,
+  dutiesManager,
+  userManager,
+  telegramBot
+}
+
 gsheet.open()
-  .then(() => userManager.getUser('jj'))
-  .then(user => telegramBot.sendMessage(user, 'Hello World'))
-  .then(response => {
-    console.log(response)
-  })
+  .then(() => remindDuty(DateTime.local(2018, 6, 7), dependencies))
   .catch(err => console.error(err))
